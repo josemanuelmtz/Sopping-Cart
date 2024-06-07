e68b39df8cc1bdd8abc79fd29f5db930b5e152c8
josemanuelmtz <martinezgarciajosemanuel225@gmail.com>
2024-06-07 16:59:02 -0600

    Creación del Login

---
+++ src/app/componentes/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required, Validators.pattern('^(?=.[a-z])(?=.[A-Z])(?=.[!@#$%^&])(?=.*\\d).{8,}$')]
+  })
+
+  constructor(private fb:FormBuilder){
+    
+  }
+
+  get email(){
+    return this.loginForm.controls['email'];
+  }
+  
+  get password(){
+    return this.loginForm.controls['password'];
+  }
+ 
+
+}
+
